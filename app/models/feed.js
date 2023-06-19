5428ef8c62e53861e139c807935e27788fe89875
Jayadeep JK <jayadeep@zeliot.in>
2023-06-19 19:31:59 +0530

    feed model defined

---
+++ app/models/feed.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../config/database");
+
+const Feed = sequelize.define("Feed", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+});
+
+module.exports = Feed;
